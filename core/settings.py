d38b4677d0ffe284555b6944816e398fb5d3a558
jollypolly123 <rachelle.hu123@gmail.com>
2020-08-24 12:02:32 -0400

    settings

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=False)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['localhost', '.herokuapp.com', '127.0.0.1', config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
